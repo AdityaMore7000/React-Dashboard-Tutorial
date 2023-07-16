48b1b81b2ea8541eee7abd2c4b03e7ec05aa25c6
AdityaMore7000 <adi.more7000@gmail.com>
2023-07-16 21:16:06 +0530

    dark

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <h1 className=" bg-slate-500 w-fit h-fit text-3xl p-3 rounded-xl transition-all duration-500 hover:bg-slate-300 absolute m-auto inset-0">Hello</h1>
     </>
   )
 }
